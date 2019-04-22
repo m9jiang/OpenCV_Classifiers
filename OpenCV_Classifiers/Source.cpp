811276276b0d61a637a175aabd9b979e462cd641
Mingzhe Jiang <m9jiang@gmail.com>
2019-04-22 09:29:58 -0400

    Bash processing for all images in different goders

--- OpenCV_Classifiers/Source.cpp
+++ OpenCV_Classifiers/Source.cpp
@@ -60,7 +60,8 @@ read_num_class_data(const string& dataname, const string& labelname,
 		responses.push_back(temp);
 	}
 	Mat(responses).copyTo(*_responses);
-	//For non-numerical label
+	
+	////For non-numerical label
 	//vector<int> responses;
 	//string currentlabel;
 	//ifstream p_label;
@@ -161,43 +162,43 @@ static void test_and_save_classifier(const Ptr<StatModel>& model,
 {
 	int i, nsamples_all = data.rows;
 	double train_hr = 0, test_hr = 0;
-	//ofstream result_file("D:/DATA/Journal_Date/First9scenes/20100418_163315/feature_image_pile/RF_result.data");
+	
 
+	ofstream result_file("D:/DATA/Journal_Date/21scenes/data/20100418_163315/RF_LOO_result.data");
 
-
-	//// compute prediction error on train and test data
-	//for (i = 0; i < nsamples_all; i++)
-	//{
-	//	Mat sample = data.row(i);
-
-	//	float r = model->predict(sample);
-	//	//r = std::abs(r + rdelta - responses.at<int>(i)) <= FLT_EPSILON ? 1.f : 0.f;
-
-	//	//if (i < ntrain_samples)
-	//	//	train_hr += r;
-	//	//else
-	//	//	test_hr += r;
-	//	result_file << r << endl;
-	//}
-	//result_file.close();
-
+	// compute prediction error on train and test data
 	for (i = 0; i < nsamples_all; i++)
 	{
 		Mat sample = data.row(i);
 
 		float r = model->predict(sample);
-		r = std::abs(r + rdelta - responses.at<int>(i)) <= FLT_EPSILON ? 1.f : 0.f;
+		//r = std::abs(r + rdelta - responses.at<int>(i)) <= FLT_EPSILON ? 1.f : 0.f;
 
-		if (i < ntrain_samples)
-			train_hr += r;
-		else
-			test_hr += r;
+		//if (i < ntrain_samples)
+		//	train_hr += r;
+		//else
+		//	test_hr += r;
+		result_file << r << endl;
 	}
+	result_file.close();
+
+	//for (i = 0; i < nsamples_all; i++)
+	//{
+	//	Mat sample = data.row(i);
+
+	//	float r = model->predict(sample);
+	//	r = std::abs(r + rdelta - responses.at<int>(i)) <= FLT_EPSILON ? 1.f : 0.f;
+
+	//	if (i < ntrain_samples)
+	//		train_hr += r;
+	//	else
+	//		test_hr += r;
+	//}
 
 	test_hr /= nsamples_all - ntrain_samples;
 	train_hr = ntrain_samples > 0 ? train_hr / ntrain_samples : 1.;
 
-	printf("Recognition rate: train = %.1f%%, test = %.1f%%\n",
+	printf("Recognition rate: train = %.2f%%, test = %.2f%%\n",
 		train_hr*100., test_hr*100.);
 
 	if (!filename_to_save.empty())
@@ -205,6 +206,104 @@ static void test_and_save_classifier(const Ptr<StatModel>& model,
 		model->save(filename_to_save);
 	}
 }
+
+static void test_SVM_RF_classifier(const Ptr<StatModel>& model,
+	const Mat& data, string SceneID_single, string classifier_name)
+{
+	int i, j;
+	int nsamples_all = data.rows;
+	
+	//ofstream result_file;
+	//if (classifier_name == "RF")
+	//{
+	//	result_file.open("D:/DATA/Journal_Date/21scenes/data/" + SceneID_single + "/RF_LOO_result.data");
+	//}
+
+	//if (classifier_name == "SVM")
+	//{
+	//	result_file.open("D:/DATA/Journal_Date/21scenes/data/" + SceneID_single + "/SVM_LOO_result_OpenCV.data");
+	//}
+	Mat im = imread("D:/DATA/Journal_Date/21scenes/data/" + SceneID_single + "/imagery_HH4_by_4average.tif");
+	int height = im.rows;
+	int width = im.cols;
+	Mat result_im = Mat(height, width, CV_8U);
+	Mat result_im_color = Mat(height, width, CV_8UC3);
+	//Mat lanmask = imread("D:/DATA/Journal_Date/21scenes/data/" + SceneID_single + "/landmask.bmp");
+	//Mat result_im;
+	//// compute prediction error on train and test data
+	//for (i = 0; i < nsamples_all; i++)
+	//{
+	//	Mat sample = data.row(i);
+	//	int r = (int)model->predict(sample);
+	//	result_file << r << endl;
+	//}
+	//result_file.close();
+		// compute prediction error on train and test data
+	
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++) 
+		{
+			Mat sample = data.row(i*height+j);
+			double temp = model->predict(sample);
+			unsigned char  temp_uchar = (unsigned char)temp;
+			//int temp_int = (int)temp;
+			result_im.at<unsigned char>(j, i) = temp_uchar;
+			//switch (temp_uchar)
+			//{
+			//case 1:
+			//	result_im_color.at<Vec3b>(j, i)[2] = 170;
+			//	result_im_color.at<Vec3b>(j, i)[1] = 40;
+			//	result_im_color.at<Vec3b>(j, i)[0] = 240;
+			//	break;
+			//case 2:
+			//	result_im_color.at<Vec3b>(j, i)[2] = 255;
+			//	result_im_color.at<Vec3b>(j, i)[1] = 255;
+			//	result_im_color.at<Vec3b>(j, i)[0] = 0;
+			//	break;
+			//case 3:
+			//	result_im_color.at<Vec3b>(j, i)[2] = 255;
+			//	result_im_color.at<Vec3b>(j, i)[1] = 0;
+			//	result_im_color.at<Vec3b>(j, i)[0] = 0;
+			//	break;
+			//case 4:
+			//	result_im_color.at<Vec3b>(j, i)[2] = 150;
+			//	result_im_color.at<Vec3b>(j, i)[1] = 200;
+			//	result_im_color.at<Vec3b>(j, i)[0] = 255;
+			//	break;
+			//default:
+			//	result_im_color.at<Vec3b>(j, i)[2] = 0;
+			//	result_im_color.at<Vec3b>(j, i)[1] = 0;
+			//	result_im_color.at<Vec3b>(j, i)[0] = 0;
+			//}
+		}
+	}
+	
+	
+	
+	//for (i = 0; i < nsamples_all; i++)
+	//{
+	//	Mat sample = data.row(i);
+	//	int r = (int)model->predict(sample);
+	//	result_im.push_back(r);
+	//}
+
+	//result_im.reshape(0, height);
+
+	if (classifier_name == "RF")
+	{
+		imwrite("D:/DATA/Journal_Date/21scenes/data/" + SceneID_single + "/RF_LOO_result.bmp", result_im);
+		//imwrite("D:/DATA/Journal_Date/21scenes/data/" + SceneID_single + "/RF_LOO_result_color.bmp", result_im_color);
+	}
+
+	if (classifier_name == "SVM")
+	{
+		imwrite("D:/DATA/Journal_Date/21scenes/data/" + SceneID_single + "/SVM_LOO_result_OpenCV.bmp", result_im);
+		//imwrite("D:/DATA/Journal_Date/21scenes/data/" + SceneID_single + "/SVM_LOO_result_OpenCV_color.bmp", result_im_color);
+	}
+	result_im.release();
+}
+
 
 
 static bool
@@ -271,6 +370,36 @@ build_rtrees_classifier(const string& data_filename, const string& label_filenam
 			printf("\t%-5.3f\n", 100.f*var_importance.at<float>(i) / rt_imp_sum);
 	}
 
+	return true;
+}
+
+static bool
+test_classifier(const string& data_filename, const string& rf_to_load, const string& svm_to_load, string SceneID_single)
+{
+	Mat data;
+	Mat responses;
+	string label_filename = "";
+	bool ok = read_num_class_data(data_filename, label_filename, &data, &responses);
+	if (!ok)
+		return ok;
+
+	Ptr<RTrees> rf_model;
+	Ptr<SVM> svm_model;
+
+	int nsamples_all = data.rows;
+	int ntrain_samples = nsamples_all;
+
+	rf_model = load_classifier<RTrees>(rf_to_load);
+	if (rf_model.empty())
+		return false;
+
+	svm_model = load_classifier<SVM>(svm_to_load);
+	if (svm_model.empty())
+		return false;
+	string rf_name = "RF";
+	string svm_name = "SVM";
+	test_SVM_RF_classifier(rf_model, data, SceneID_single, rf_name);
+	test_SVM_RF_classifier(svm_model, data, SceneID_single, svm_name);
 	return true;
 }
 
@@ -588,8 +717,8 @@ int main(int argc, char *argv[])
 {
 	string filename_to_save = "";
 	string filename_to_load = "";
-	string data_filename;
-	string label_filename;
+	string data_filename = "";
+	string label_filename = "";
 	int method = 0;
 
 	//The format of label file must be .csv. Tried .label and could not be loaded correctly. Why?
@@ -623,26 +752,91 @@ int main(int argc, char *argv[])
 	//int type1 = data.type();
 	//imshow("ss",data);
 	//waitKey(0);
-	string root_path = "D:/PhD/data/Proposal_result/First9scenes7featHHHV/9features";
+	
 	//rf_features(root_path, filename_to_load)
-
 
 	//CvMLData mlData;
 	//mlData.read_csv("cameraFrame1.csv");
 	//build_rtrees_classifier(data_filename, label_filename, filename_to_save, filename_to_load);
 	//build_svm_classifier(data_filename, label_filename, filename_to_save, filename_to_load);
 
+	int Num_scene = 21;
+	//string root_path = "D:/DATA/Journal_Date/model_training/21scenes/30_features/Nornalized_Intensity+GLCM";
+	//string root_path = "D:/DATA/Journal_Date/Model_Debug/Not_sparse/";
+	//Could extern be used here?
+	string data_root_path;
+	string model_root_path;
+	string classifier;
+	data_root_path = "D:/DATA/Journal_Date/21scenes/data";
+	model_root_path = "D:/DATA/Journal_Date/model_training/21scenes/30_features/Nornalized_Intensity+GLCM";
+	int current_scene = 0;
+	string classifier_name;
+	////Model training
+	//for (int i = 0; i < Num_scene; i++)
+	//{
+	//	filename_to_save = root_path + "/Random_Forest_Trained_Models/RF_LOO_scene_"+ std::to_string(i + 1) +".model" ;
+	//	filename_to_load = "";
+	//	data_filename = root_path + "/Random_Forest_Training_Data/RF_LOO_training_for_scene_"+ std::to_string(i + 1) +".data";
+	//	label_filename = root_path + "/Random_Forest_Training_Data/RF_LOO_training_for_scene_"+ std::to_string(i + 1) +".label";
+	//	build_rtrees_classifier(data_filename, label_filename, filename_to_save, filename_to_load);
+	//}
 
+	//for (int i = 0; i < Num_scene; i++)
+	//{
+	//	filename_to_save = root_path + "/SVM/OpenCV_models/SVM_LOO_scene_" + std::to_string(i + 1) + ".model";
+	//	filename_to_load = "";
+	//	data_filename = root_path + "/Random_Forest_Training_Data/RF_LOO_training_for_scene_" + std::to_string(i + 1) + ".data";
+	//	label_filename = root_path + "/Random_Forest_Training_Data/RF_LOO_training_for_scene_" + std::to_string(i + 1) + ".label";
+	//	build_svm_classifier(data_filename, label_filename, filename_to_save, filename_to_load);
+	//}
 
-	string filename_to_save_0 = "";
-	string filename_to_load_0 = "";
-	string data_filename_0 = "D:/DATA/Journal_Date/model_training/21scenes/RF_feature_importance/Selected_30_features/Inten_Selected_30_GLCM.csv";
-	string label_filename_0 = "D:/DATA/Journal_Date/model_training/21scenes/RF_feature_importance/Selected_30_features/label.csv";
-	//build_rtrees_classifier(data_filename_0, label_filename_0, filename_to_save_0, filename_to_load_0);
-	build_svm_classifier(data_filename_0, label_filename_0, filename_to_save_0, filename_to_load_0);
+	////classifiy whole image using trained model and feature pile.
+	//filename_to_load = model_root_path + "/Random_Forest_Trained_Models/RF_LOO_scene_1.model";
+	//data_filename = data_root_path + "/20100418_163315/feature_image_pile/All_30_features.data";
+	//build_rtrees_classifier(data_filename, label_filename, filename_to_save, filename_to_load);
 
+	////SVM grid search
+	//data_filename = root_path + "/30_features_all_21_scenes.data";
+	//label_filename = root_path + "/30_features_all_21_scenes.label";
+	//build_svm_classifier(data_filename, label_filename, filename_to_save, filename_to_load);
+	string SceneID_single;
+	vector <string> SceneID;
+	SceneID.push_back("20100418_163315");
+	SceneID.push_back("20100426_040439");
+	SceneID.push_back("20100510_035620");
+	SceneID.push_back("20100524_034756");
+	SceneID.push_back("20100605_163323");
+	SceneID.push_back("20100623_041255");
+	SceneID.push_back("20100629_163326");
+	SceneID.push_back("20100712_031834");
+	SceneID.push_back("20100721_173208");
+	SceneID.push_back("20100730_162908");
+	SceneID.push_back("20100807_173610");
+	SceneID.push_back("20100816_163329");
+	SceneID.push_back("20100907_035614");
+	SceneID.push_back("20100909_163321");
+	SceneID.push_back("20101003_163324");
+	SceneID.push_back("20101021_041325");
+	SceneID.push_back("20101027_025726");
+	SceneID.push_back("20101114_041304");
+	SceneID.push_back("20101120_163324");
+	SceneID.push_back("20101206_015139");
+	SceneID.push_back("20101214_025725");
 
+	//Model Test
+	for (int i = 0; i < Num_scene; i++)
+	{
+		string filename_rf_model = model_root_path + "/Random_Forest_Trained_Models/RF_LOO_scene_"+ std::to_string(i + 1) +".model" ;
+		string filename_svm_model = model_root_path + "/SVM/OpenCV_models/SVM_LOO_scene_" + std::to_string(i + 1) + ".model";
+		SceneID_single = SceneID[i];
+		data_filename = data_root_path + "/" + SceneID[i] + "/feature_image_pile/All_30_features.data";
 
+		//build_svm_classifier(data_filename, label_filename, filename_to_save, filename_svm_model);
+		//build_rtrees_classifier(data_filename, label_filename, filename_to_save, filename_rf_model);
+
+		test_classifier(data_filename, filename_rf_model, filename_svm_model, SceneID_single);
+
+	}
 
 
 	return 0;
